024b5699285464df936bc11eaa0fe50913abb60a
Radu Ciorba <radu@devrandom.ro>
2017-05-17 13:16:03 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ try:
     import pypandoc
     long_description = pypandoc.convert('README.md', 'rst')
 except(IOError, ImportError):
-    long_description = open('README.md').read()
+    long_description = "Page Object Model for Selenium"
 
-
+tests_require = ["pytest"]
 setup(
     name='selenium-pom',
-    version='0.1.0',
+    version='0.1.1',
     description="Page Object Model for Selenium",
     author="Presslabs SRL",
     author_email="ping@presslabs.com",
     url="https://github.com/Presslabs/selenium-pom",
     install_requires=["selenium"],
-    tests_require=["pytest"],
+    tests_require=tests_require,
     packages=find_packages(exclude=['tests']),
     extras_require={
         'test': tests_require
