343ac19a1c0180f3cf40e1109668df0e487afda1
Radu Ciorba <radu@devrandom.ro>
2017-07-28 13:30:21 +0300

    fixed wait_* hanging on stale parent element

--- selenium_pom/page.py
+++ selenium_pom/page.py
@@ -1,10 +1,13 @@
 from __future__ import print_function
 
+import os
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
+from selenium.common.exceptions import TimeoutException
 
 import logging
 
@@ -34,6 +37,14 @@ class Element(object):
             self._locator = None  # will be copied from the prototype, see new
         else:
             self._locator = get_locator(kwargs)
+        if parent is not None:
+            self.wait_visible()
+
+    def _repr_locator(self):
+        return self.parent._repr_locator() + " / {!r}:{!r}".format(*self._locator)
+
+    def __repr__(self):
+        return "{}".format(self._repr_locator())
 
     def __get__(self, obj, objtype):
         return self.new(parent=obj)
@@ -46,21 +57,64 @@ class Element(object):
         return new_element
 
     @property
+    def driver(self):
+        return self.parent.driver
+
+    @property
+    def element(self):
+        return self.parent.find_element(*self._locator)
+
+    @property
     def timeout(self):
         return self.parent.timeout
 
-    def wait_visible(self):
+    def is_visible(self, driver):
+        if isinstance(self.parent, Element):
+            parent_el = self.parent.element
+        else:
+            parent_el = self.parent
+        el = self.element
+        if el is None:
+            return False
+        else:
+            return el
+
+    def is_present(self, driver):
+        if isinstance(self.parent, Element):
+            parent_el = self.parent.element
+        else:
+            parent_el = self.parent
+        el = self.element
+        if el is None:
+            return False
+        else:
+            return el
+
+    def wait_visible(self, timeout=None):
         """Wait for the element to be visible"""
-        el =  WebDriverWait(self.parent, self.timeout).until(
-            EC.presence_of_element_located(self._locator))
-        el = WebDriverWait(self.parent, self.parent.timeout).until(
-            EC.visibility_of(el)
-        )
+        if timeout is None:
+            timeout = self.timeout
+        if isinstance(self.parent, Element):
+            parent_el = self.parent.element
+        else:
+            parent_el = self.parent
+        try:
+            el = WebDriverWait(parent_el, timeout).until(
+                self.is_present)
+        except TimeoutException as expt:
+            raise TimeoutException("unable to locate %r" % self)
+        try:
+            el = WebDriverWait(self.driver, timeout).until(
+                self.is_visible)
+        except TimeoutException as expt:
+            raise TimeoutException("%r not visible" % self)
         return el
 
-    def wait_clickable(self):
+    def wait_clickable(self, timeout=None):
         """Wait for the element to be clickable"""
-        return WebDriverWait(self.parent, self.parent.timeout).until(
+        if timeout is None:
+            timeout = self.parent.timeout
+        return WebDriverWait(self.driver, self.timeout).until(
             EC.element_to_be_clickable(self._locator)
         )
 
@@ -76,6 +130,10 @@ class Element(object):
         el = self.wait_clickable()
         el.send_keys(keys)
 
+    def input(self, keys):
+        self.clear()
+        self.send_keys(keys)
+
     @property
     def text(self):
         el = self.wait_visible()
@@ -83,18 +141,58 @@ class Element(object):
 
     def hover(self):
         el = self.wait_visible()
-        ActionChains(self.parent).move_to_element(el).perform()
+        ActionChains(self.driver).move_to_element(el).perform()
+
+    @property
+    def w3c(self):
+        return self.parent.w3c
 
     def __getattr__(self, attr_name):
         if self.parent is None:
             raise AttributeError(attr_name)
-        return getattr(self.parent.find_element(*self._locator), attr_name)
+        return getattr(self.element, attr_name)
+
+
+class Reloader(object):
+    def __init__(self, page):
+        self.page = page
+
+    def _getattr(self, obj, attr_name):
+        attributes = attr_name.split('.')
+        for attr in attributes:
+            obj = getattr(obj, attr)
+        return obj
+
+    def __getattr__(self, attr_name):
+        try_count = 0
+        while True:
+            try_count += 1
+            timeout = self.page.timeout / 5
+            try:
+                el = self._getattr(self.page, attr_name)
+                el.wait_visible(timeout=timeout)
+                return el
+            except:
+                if try_count >= 10:
+                    raise
+            if try_count >= 10:
+                assert False
+            self.page.goto()
 
 
 class Page(object):
-    def __init__(self, driver, timeout=10):
+    def __init__(self, driver, timeout=None):
+        if timeout is None:
+            timeout = int(os.getenv('SELENIUM_TIMEOUT', '20'))
         self.parent = driver
         self.timeout = timeout
+
+    def _repr_locator(self):
+        return ""
+
+    @property
+    def driver(self):
+        return self.parent
 
     def goto(self, url_extra=""):
         self.parent.get(self.url + url_extra)
@@ -102,3 +200,6 @@ class Page(object):
     def __getattr__(self, attr_name):
         # print attr_name
         return getattr(self.parent, attr_name)
+
+    def reload_until(self):
+        return Reloader(self)
